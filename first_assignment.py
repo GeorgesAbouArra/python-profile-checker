0c375c2710396e4919a6d7e83aed4af798f45544
georges abou arra <georgesabouarra9@gmail.com>
2025-06-10 10:38:04 +0300

    added loops
    added while loops to not end the program when something wrong happens.

--- first_assignment.py
+++ first_assignment.py
@@ -1,19 +1,26 @@
-
 #-------------inputs-----------------
-name=(str(input("enter your name: ")))
-age=(int(input("enter your age:")))
-gpa=(float(input("enter your GPA (0-5):")))
-feild_of_intrest=(str(input("enter your field of intrest:")))
-graduated=(str(input("are you graduated?(yes or no only)")))
+name=str(input("enter your name: "))
+age=int(input("enter your age:"))
+gpa=float(input("enter your GPA (0-5):"))
+feild_of_intrest=str(input("enter your field of intrest:"))
+graduated=str(input("are you graduated?(yes or no only)")).lower()
+#------conditions------ 
+while gpa < 0 or gpa > 5:
+    gpa=int(input("please enter a valid GPA number"))
+while graduated!="yes" and graduated!="y" and graduated!="no" and graduated!="n":
+    graduated=str(input("please enter yes or no only"))
+else:
 #-------------all info--------------
-print("-----user information-----")
-print("name:",name)
-print("age:",age)
-print("GPA:",gpa)
-print("feild of intrest:",feild_of_intrest)
-print("graduated:",graduated)
+    print("-----user information-----")
+    print("name:",name)
+    print("age:",age)
+    print("GPA:",gpa)
+    print("feild of intrest:",feild_of_intrest)
+    print("graduated:",graduated)
+if graduated =="yes" or graduated=="y":
+    print ("congrats on your graduation")
 #-----to see if eligible for scholarship-----
-if age<25 and gpa>=3.5 and graduated=="yes":
+if age<25 and gpa>=3.5 and graduated=="yes" or graduated == "y":
      print("you",name,"are eligible to a scholarship")
 #-----to see if eligible to a intership
 elif age<30 and gpa>=2.5:
